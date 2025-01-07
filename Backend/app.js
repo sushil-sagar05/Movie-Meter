7c7cdf7279761867b04ac2f47ed168727d0a5a3d
sushil-sagar05 <sagarsushil1403@gmail.com>
2025-01-08 02:02:46 +0530

    Initial Commit Setup server to Backend

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectdb = require('./DB/db')
+const app = express();
+connectdb();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+module.exports = app;
