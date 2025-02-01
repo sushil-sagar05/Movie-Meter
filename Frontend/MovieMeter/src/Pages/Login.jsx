f5e32b5f602ad33ed4863f35b5f05bf6e16731c3
sushil-sagar05 <sagarsushil1403@gmail.com>
2025-02-01 20:46:14 +0530

    fixes

--- Frontend/MovieMeter/src/Pages/Login.jsx
+++ Frontend/MovieMeter/src/Pages/Login.jsx
@@ -22,7 +22,9 @@ function Login() {
             password:password
         }
       try {
-        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/user/login`,userData)
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/user/login`,userData,{
+          withCredentials:"true",
+        })
         if(response.status===200){
           const data = response.data
           setuser(data.user)
