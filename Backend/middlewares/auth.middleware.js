38c9aa13b76b10149908f4540e44ce1bd7e6a31e
sushil-sagar05 <sagarsushil1403@gmail.com>
2025-02-02 14:04:16 +0530

    tackling problem

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -6,17 +6,22 @@ module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
   if (!token) {
+    console.log("No token provided");
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
   const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
+  console.log('Blacklisted token check:', isBlacklisted);
+
 
   if (isBlacklisted) {
+    console.log('Token is blacklisted');
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Decoded token:', decoded);
     const user = await userModel.findById(decoded._id);
 
     if (!user) {
