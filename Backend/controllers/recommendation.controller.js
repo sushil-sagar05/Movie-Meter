a6db71f4754f2928f580aae254e7f7037e6fb0d7
sushil-sagar05 <sagarsushil1403@gmail.com>
2025-05-06 00:47:25 +0530

    debugging

--- Backend/controllers/recommendation.controller.js
+++ Backend/controllers/recommendation.controller.js
@@ -1,9 +1,13 @@
 const axios  = require('axios')
 module.exports.recommendation = async (req, res, next) => {
     try {
+        console.log("APi hit")
         const user = req.user
+        console.log(user)
         const flaskURL = `${process.env.flaskURL}/recommend/${user._id}`
+        console.log(flaskURL)
         const response = await axios.get(flaskURL);
+        console.log(response)
         res.status(200).json(response.data)
     } catch (error) {
         res.status(500).json({error:"Error getting recommondations"})
